d1be7f9fbcaf7622a66cede085bec85f938a7e9f
yuval miles <yuvalmil9@gmail.com>
2024-11-20 17:11:37 +0200

    update all files

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 const dotenv = require("dotenv").config()
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-
-// mongoose.connect(process.env.DB_URL,{useNewUrlParser : true})
-// const db = mongoose.connection
-// db.on("error",(err)=>{
-// console.log(err)
-// })
-
-// db.once("open",()=>{
-//     console.log("connected to the DB")
-// })
-
+const usersRouter = require("./routes/usersusersRouter.js")
 const port = process.env.PORT
+
+mongoose.connect(process.env.DB_URL,{useNewUrlParser : true})
+const db = mongoose.connection
+db.on("error",(err)=>{
+console.log(err)
+})
+db.once("open",()=>{
+    console.log("Connected to the DB")
+})
+
+
+app.use("/users",usersRouter)
+
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended : true}))
 
-app.listen(port,()=>{
-    console.log(`The app is running at localhost${port}`)
+app.listen(port, ()=>{
+    console.log(`The App is running at localhost:${port}`)
 })
